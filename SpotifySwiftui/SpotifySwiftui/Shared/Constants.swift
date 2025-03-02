3ae7e5d5bb4f0ba099995684662fdd4ac6236508
Sabbir <62333547+sabbirn26@users.noreply.github.com>
2025-03-02 11:46:07 +0600

    - created: ImageLoaderView, Constant file in shared folder

---
+++ SpotifySwiftui/SpotifySwiftui/Shared/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  SpotifySwiftui
+//
+//  Created by Sabbir Nasir on 2/3/25.
+//
+
+import Foundation
+
+struct Constants {
+    static let randomImage = "https://picsum.photos/600/600"
+}
