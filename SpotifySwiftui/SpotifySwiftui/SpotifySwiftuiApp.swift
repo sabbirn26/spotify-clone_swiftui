9e644476ebffcfdc8eea0841cd38e5aeec81295e
Sabbir <62333547+sabbirn26@users.noreply.github.com>
2025-03-02 11:27:41 +0600

    - init commit: project setup and added packages

---
+++ SpotifySwiftui/SpotifySwiftui/SpotifySwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifySwiftuiApp.swift
+//  SpotifySwiftui
+//
+//  Created by Sabbir Nasir on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SpotifySwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
