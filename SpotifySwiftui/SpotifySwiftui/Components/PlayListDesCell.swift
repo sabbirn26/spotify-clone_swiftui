43e57a1cbfd1ac1e83f2c40b3a41d8f20e5adc8b
Sabbir <62333547+sabbirn26@users.noreply.github.com>
2025-03-06 12:03:39 +0600

    - created: six image and added ontapgesture on them to act like button

--- SpotifySwiftui/SpotifySwiftui/Components/PlayListDesCell.swift
+++ SpotifySwiftui/SpotifySwiftui/Components/PlayListDesCell.swift
@@ -25,22 +25,79 @@ struct PlayListDesCell: View {
                 .foregroundStyle(.spLightGray)
                 .frame(maxWidth: .infinity, alignment: .leading)
             
-            HStack(spacing: 8,content: {
-                Image(systemName: "applelogo")
-                    .font(.title3)
-                    .foregroundStyle(.spGreen)
-                
-                Text("Made for ")
-                +
-                Text(crUserName)
-                    .bold()
-                    .foregroundStyle(.spWhite)
-                
-            })
+            madeForYouSection
+            Text(subheadline)
+            
+            HStack(spacing: 0){
+                HStack(spacing: 0){
+                    Image(systemName: "plus.circle")
+                        .padding(8)
+                        .background(Color.black.opacity(0.001))
+                        .onTapGesture {
+                            //code here
+                        }
+                    Image(systemName: "arrow.down.circle")
+                        .padding(8)
+                        .background(Color.black.opacity(0.001))
+                        .onTapGesture {
+                            //code here
+                        }
+                    Image(systemName: "square.and.arrow.up")
+                        .padding(8)
+                        .background(Color.black.opacity(0.001))
+                        .onTapGesture {
+                            //code here
+                        }
+                    Image(systemName: "ellipsis")
+                        .padding(8)
+                        .background(Color.black.opacity(0.001))
+                        .onTapGesture {
+                            //code here
+                        }
+                    
+                }
+                .offset(x: -8)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                HStack(spacing: 8){
+                    Image(systemName: "shuffle")
+                        .font(.system(size: 24))
+                        .background(Color.black.opacity(0.001))
+                        .onTapGesture {
+                            //code here
+                        }
+                    
+                    Image(systemName: "play.circle.fill")
+                        .font(.system(size: 45))
+                        .background(Color.black.opacity(0.001))
+                        .onTapGesture {
+                            //code here
+                        }
+                }
+                .foregroundStyle(.spGreen)
+            }
+            .font(.title2)
         })
         .font(.callout)
         .fontWeight(.medium)
         .foregroundStyle(.spLightGray)
+    }
+}
+
+extension PlayListDesCell {
+    //MARK: View Parts
+    private var madeForYouSection : some View {
+        HStack(spacing: 8,content: {
+            Image(systemName: "applelogo")
+                .font(.title3)
+                .foregroundStyle(.spGreen)
+            
+            Text("Made for ")
+            +
+            Text(crUserName)
+                .bold()
+                .foregroundStyle(.spWhite)
+            
+        })
     }
 }
 
