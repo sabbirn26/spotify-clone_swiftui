85f3e954cecdf8e637a7b449fd016a3aef8859df
Sabbir <62333547+sabbirn26@users.noreply.github.com>
2025-03-06 12:36:03 +0600

    - refactored: added cell to the playlist view

--- SpotifySwiftui/SpotifySwiftui/Core/SpotifyPlaylistView.swift
+++ SpotifySwiftui/SpotifySwiftui/Core/SpotifyPlaylistView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct SpotifyPlaylistView: View {
     var product: Product = .mock
+    var user: User = .mock
     var body: some View {
         ZStack{
             Color.spBlack.ignoresSafeArea()
@@ -16,6 +17,25 @@ struct SpotifyPlaylistView: View {
             ScrollView(.vertical) {
                 LazyVStack(spacing: 12) {
                     PlayListHeaderCell(height: 250, title: product.title, subtitle: product.brand ?? "", imageName: product.thumbnail)
+                    
+                    PlayListDesCell(
+                        descriptionText: product.description,
+                        crUserName: user.lastName,
+                        subheadline: product.category) {
+                            //code here
+                        } onDownloadPressed: {
+                            //code here
+                        } onSharePressed: {
+                            //code here
+                        } onElipsisPressed: {
+                            //code here
+                        } onSufflePressed: {
+                            //code here
+                        } onPlayPressed: {
+                            //code here
+                        }
+                        .padding(.horizontal, 16)
+
                 }
             }
         }
