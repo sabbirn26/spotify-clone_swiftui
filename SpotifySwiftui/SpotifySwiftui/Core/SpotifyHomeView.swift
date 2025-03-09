bbcc10887213731bb3ea102abf46896c95d3fb37
Sabbir <62333547+sabbirn26@users.noreply.github.com>
2025-03-09 13:01:49 +0600

    -reduced: extra fetch request

--- SpotifySwiftui/SpotifySwiftui/Core/SpotifyHomeView.swift
+++ SpotifySwiftui/SpotifySwiftui/Core/SpotifyHomeView.swift
@@ -48,6 +48,7 @@ struct SpotifyHomeView: View {
     }
     
     private func getData() async {
+        guard products.isEmpty else {return}
         do {
             currentUser = try await DatabaseHelper().getUsers().first
             products = try await Array(DatabaseHelper().getProducts().prefix(8))
