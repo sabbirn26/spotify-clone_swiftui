93d3d91cf63d56545b5d3f0e94438899886f5ce7
Sabbir <62333547+sabbirn26@users.noreply.github.com>
2025-03-13 15:28:29 +0600

    refactored: DatabaseHelper

--- SpotifySwiftui/SpotifySwiftui/Shared/DatabaseHelper.swift
+++ SpotifySwiftui/SpotifySwiftui/Shared/DatabaseHelper.swift
@@ -7,24 +7,41 @@
 
 import Foundation
 
+// A helper struct for fetching data from an API
 struct DatabaseHelper {
-    func getProducts() async throws -> [Product] {
-            guard let url = URL(string: "https://dummyjson.com/products") else {
-                throw URLError(.badURL)
-            }
-            
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let products = try JSONDecoder().decode(ProductArray.self, from: data)
-            return products.products
-        }
     
-    func getUsers() async throws -> [User] {
-            guard let url = URL(string: "https://dummyjson.com/users") else {
-                throw URLError(.badURL)
-            }
-            
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let users = try JSONDecoder().decode(UserArray.self, from: data)
-            return users.users
+    // Fetches a list of products from the API
+    func getProducts() async throws -> [Product] {
+        // Ensure the URL is valid
+        guard let url = URL(string: "https://dummyjson.com/products") else {
+            throw URLError(.badURL) // Throw an error if the URL is invalid
         }
+        
+        // Fetch data asynchronously using URLSession
+        let (data, _) = try await URLSession.shared.data(from: url)
+        
+        // Decode the JSON response into a `ProductArray` struct
+        let products = try JSONDecoder().decode(ProductArray.self, from: data)
+        
+        // Return the list of products
+        return products.products
+    }
+    
+    // Fetches a list of users from the API
+    func getUsers() async throws -> [User] {
+        // Ensure the URL is valid
+        guard let url = URL(string: "https://dummyjson.com/users") else {
+            throw URLError(.badURL) // Throw an error if the URL is invalid
+        }
+        
+        // Fetch data asynchronously using URLSession
+        let (data, _) = try await URLSession.shared.data(from: url)
+        
+        // Decode the JSON response into a `UserArray` struct
+        let users = try JSONDecoder().decode(UserArray.self, from: data)
+        
+        // Return the list of users
+        return users.users
+    }
 }
+
