bcdf0df201c08b23eabd96b547635f3b33184328
Sabbir <62333547+sabbirn26@users.noreply.github.com>
2025-03-13 15:28:41 +0600

    refactored: ImageLoaderView

--- SpotifySwiftui/SpotifySwiftui/Shared/ImageLoaderView.swift
+++ SpotifySwiftui/SpotifySwiftui/Shared/ImageLoaderView.swift
@@ -6,27 +6,31 @@
 //
 
 import SwiftUI
-import SDWebImageSwiftUI
+import SDWebImageSwiftUI // Importing the library for loading images asynchronously
+
+// A SwiftUI view for loading and displaying an image from a URL
 struct ImageLoaderView: View {
-    var urlString: String = Constants.randomImage
-    var resizingMode: ContentMode = .fill
+    var urlString: String = Constants.randomImage // Default image URL if none is provided
+    var resizingMode: ContentMode = .fill // Image content mode (default is `.fill`)
+    
     var body: some View {
-        Rectangle()
-            .opacity(0.001)
-            .overlay (
-                WebImage(url: URL(string: urlString))
-                    .resizable()
-                    .indicator(.activity)
-                    .aspectRatio(contentMode: resizingMode)
+        Rectangle() // Placeholder rectangle for layout purposes
+            .opacity(0.001) // Makes the rectangle almost invisible
+            .overlay( // Overlaying the image on top of the rectangle
+                WebImage(url: URL(string: urlString)) // Load image from URL
+                    .resizable() // Allow the image to resize
+                    .indicator(.activity) // Show a loading indicator while fetching the image
+                    .aspectRatio(contentMode: resizingMode) // Set aspect ratio based on provided mode
             )
-            .clipped()
-        
+            .clipped() // Clip the image to fit the parent container
     }
 }
 
+// SwiftUI Preview for testing ImageLoaderView with rounded corners and padding
 #Preview {
     ImageLoaderView()
-        .cornerRadius(30)
-        .padding(40)
-        .padding(.vertical, 60)
+        .cornerRadius(30) // Apply rounded corners
+        .padding(40) // Apply padding on all sides
+        .padding(.vertical, 60) // Additional vertical padding
 }
+
